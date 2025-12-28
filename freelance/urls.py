a46009fa002c1e2570494c0ffccd5e1e2e544d84
aigerim2006 <aigerimhalbekova@gmail.com>
2025-12-29 04:17:57 +0600

    added homework 2

--- freelance/urls.py
+++ freelance/urls.py
@@ -1,7 +1,12 @@
+from django.contrib import admin
 from django.urls import path
-from .views import home
+
+import freelance.views
 
 urlpatterns = [
-    path('', home),
+    path("admin/", admin.site.urls),
+    path("", freelance.views.home),
+    path("posts/", freelance.views.post_list),
+    path("posts/<int:post_id>/", freelance.views.post_detail),
 ]
 
--- freelance/views.py
+++ freelance/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
 
 def home(request):
     return render(request, 'index.html')
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts.html', {'posts' : posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id = post_id)
+    return render(request, 'post_detail.html', {'post' : post})
 
